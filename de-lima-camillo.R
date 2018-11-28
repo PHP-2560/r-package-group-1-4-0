3ef1de7dc348d89bd5ba88ce250a36abd0bd18b0
Lucas Camillo <lucas_camillo@brown.edu>
2018-11-28 15:30:03 -0500

    update

--- de-lima-camillo.R
+++ de-lima-camillo.R
@@ -28,11 +28,11 @@ setup_tweets = function(consumer_key = "kmp7IUYAhZQftCrYkFSICCjuz",
                         access_token = "1060241795240599557-Qyvx7TY0kYQ2ovPLYnMi6E4GNgirko",
                         access_secret = "Q0AEBJvFx8kAh1p5tOCFppWppT12Mm8iEEj0zaf7dFvxL"){
   check_packages(c("twitteR", "base64enc"))
-  setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+  invisible(capture.output(setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)))
 }
 
 #This function gets the positivity associate with the tweets related to a word from -4 to 4. There are some parameters and prints a ttest.
-get_twitter_feelings = function(string, n_tweets = 100, since_date = Sys.Date() - 30, ttest = TRUE){
+get_twitter_feelings = function(string, n_tweets = 20, since_date = Sys.Date() - 30, ttest = TRUE){
   setup_tweets()
   check_packages(c("tidytext", "stringr", "twitteR", "utf8"))
   twt = twitteR::searchTwitter(string, n = n_tweets, since = as.character(since_date), retryOnRateLimit = 1e4, lang = "en") 
@@ -81,9 +81,10 @@ get_twitter_feelings = function(string, n_tweets = 100, since_date = Sys.Date() 
   cat(utf8_format(emoji))
 }
 
+#shows one tweet
 show_tw = function(string, n_tweets = 1){
-  invisible(capture.output(setup_tweets()))
-  check_packages(c("tidytext", "twitteR"))
+  setup_tweets()
+  check_packages("twitteR")
   twt = twitteR::searchTwitter(string, n = n_tweets, since = as.character(Sys.Date() - 30))
   df_tw = twitteR::twListToDF(twt)
   print(df_tw$text)
