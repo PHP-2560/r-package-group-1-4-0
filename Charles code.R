b913e371b6481cd4e02ddba497744702c03ebaec
Charles Steele <charles_steele@brown.edu>
2018-11-28 13:51:04 -0500

    notes

---
+++ Charles code.R
@@ -0,0 +1,8 @@
+# plotting a map showing the location of a specficic university
+# -> package called map, given name of university, it goes to df and then plots it on a map
+
+# right before the function, comment all the packages that are used
+# make function as general as possible
+# call the name of the column rather than the index
+
+# given a salary, how many years would it take to pay tuition of university if interest rate is x%, how much you're giving 
