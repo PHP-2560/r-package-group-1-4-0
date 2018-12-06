5e0e45fa820114f16089f4744bb7311a4a3ac6f4
Charles Steele <charles_steele@brown.edu>
2018-12-05 23:47:35 -0500

    progress

--- Charles code.R
+++ Charles code.R
@@ -4,10 +4,12 @@
 
 #install.packages("maps")
 #install.packages("usmap")
+install.packages("ggmap")
 library(usmap)
 library(maps)
 library(ggplot2)
 library(dplyr)
+library(ggmap)
 
 # rownames(df) = c("University", 
 #                  "Year_Founded", 
@@ -73,6 +75,8 @@ plot_usmap(data = df.grouped, values = "rankLow", lines = "red") +
   scale_fill_continuous(name = "Best School Rank", low = "blue", high = "white", label = scales::comma) + 
   theme(legend.position = "right")
 
+
+
 # --------
 
 mapPlot <- function(SchoolName) {
@@ -97,6 +101,79 @@ mapPlot <- function(SchoolName) {
 # given a salary, how many years would it take to pay tuition of university if interest rate is x%, how much you're giving 
 
 # distance between home location and university
+
+
+
+
+
+LA <- map_data("usa")
+
+salesCalls <- data.frame(State=rep("louisiana",5), 
+                         City=c("Baton Rouge", "New Orleans", "Shreveport", 
+                                "Lafayette", "Mandeville"),
+                         Calls=c(10,5,8,13,2))
+
+salesCalls <- cbind(geocode(as.character(salesCalls$City), source = "dsk"), salesCalls)
+
+salesCalls
+#         lon      lat     State        City Calls
+# 1 -91.14032 30.45828 louisiana Baton Rouge    10
+# 2 -90.07153 29.95107 louisiana New Orleans     5
+# 3 -93.75018 32.52515 louisiana  Shreveport     8
+# 4 -92.01984 30.22409 louisiana   Lafayette    13
+# 5 -90.06563 30.35825 louisiana  Mandeville     2
+
+ggplot(LA, aes(x=long, y=lat)) +
+  geom_polygon() +
+  coord_map() +
+  geom_point(data =salesCalls, aes(x=lon, y=lat), color="orange")
+
+
+# blank black USA map
+# usa <- map_data("usa") # we already did this, but we can do it again
+# ggplot() + geom_polygon(data = usa, aes(x=long, y = lat, group = group)) + 
+#   coord_fixed(1.3)
+
+################################################################################################################
+
+#Parsing location and making a separate column for city
+for (i in 1:length(df.copy$location)) {
+df.copy$city[i] = strsplit(df.copy$location, split = ",")[[i]][1] 
+}
+
+#this finds the lat and lon of each city, only run ONCE
+# df.copy <- cbind(df.copy, geocode(as.character(df.copy$city), source = "dsk"))
+
+states <- map_data("state")
+
+
+
+  
+coordinates <- df.copy %>%
+    filter(universities == schoolName) %>%
+    select(lon, lat)
+
+  
+ggplot(data = states) + 
+  geom_polygon(aes(x = long, y = lat, fill = region, group = group), color = "white") + 
+  coord_fixed(1.3) +
+  guides(fill=FALSE) + geom_point(aes(x = df.copy$lon[1], y = df.copy$lat[1]))
+  
+
+
+
+
+
+
+
+#########################################################################################################
+
+
+
+
+
+
+
 
 
 
