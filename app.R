f00ce66354afd89d8f998acb7c793ee60ed0809d
William Steelegit config --global user.emailgit config --global user.name git congig --list --globalgit config --global user.name William <williamsteele1031@gmail.com>
2018-12-06 16:59:55 -0500

    progress

--- app.R
+++ app.R
@@ -1,5 +1,31 @@
 library(shiny)
-ui <- fluidPage()
-server <- function(input, output) {}
+
+ui <- fluidPage(
+  titlePanel("Group 1.0 version WEEK12"),
+  sidebarLayout(
+    sidebarPanel( br(), br(), br(),
+      "This is where are the INPUTS will be" ,
+      br(), br(), 
+      sliderInput(
+        "num", "Choose a number",
+        min = 0, max = 100,
+        value = 20),
+      plotOutput("myplot")
+      ),
+    mainPanel(br(), br(), br(),
+      "This is where all the OUTPUT and Beautiful Visuals will go"
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  output$myplot <- renderPlot({
+    plot(rnorm(input$num))
+  }) 
+  
+}
+
+
 shinyApp(ui = ui, server = server)
 
