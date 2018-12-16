26c009ca399140f5af8903d30adf7871f749ec29
William Steelegit config --global user.emailgit config --global user.name git congig --list --globalgit config --global user.name William <williamsteele1031@gmail.com>
2018-12-16 12:24:04 -0500

    progress on shiny

--- app.R
+++ app.R
@@ -1,37 +1,72 @@
+#install.packages("shiny")
+
 library(shiny)
+library(ggplot2)
 
-
-
-#Shiny app College and Universities Project
-
-
-
+# Define UI for application
 ui <- fluidPage(
-  titlePanel("Group 1.0 v.13 Shiny App Project: United States Colleges and Universities"),
+  
+  # Sidebar layout with a input and output definitions
   sidebarLayout(
-    sidebarPanel( br(), br(), br(),
-      "This is where are the INPUTS will be" ,
-      br(), br(), 
-      sliderInput( 
-        "num", "Choose a number",
-        min = 0, max = 100,
-        value = 20),
-      plotOutput("myplot")
-      ),
-    mainPanel(br(), br(), br(),
-      "This is where all the OUTPUT and Beautiful Interactice Visuals will go"
+    
+    # Inputs
+    sidebarPanel(
+      
+      # Select variable for y-axis
+      selectInput(inputId = "y", 
+                  label = "Y-axis:",
+                  choices = c("Endowment", "Median_Start_Sal", "Acc_Rate", "Score", "Tuition"), 
+                  selected = "Score"),
+      
+      # Select variable for x-axis
+      selectInput(inputId = "x", 
+                  label = "X-axis:",
+                  choices = c("Endowment", "Median_Start_Sal", "Acc_Rate", "Score", "Tuition"), 
+                  selected = "Acc_Rate"),
+      
+      # Select variable for color
+      selectInput(inputId = "z", 
+                  label = "Color by:",
+                  choices = c("School_Type", "Religion"),
+                  selected = "School_Type")
+    ),
+    
+    # Outputs
+    mainPanel(
+      plotOutput(outputId = "scatterplot"),
+      plotOutput(outputId = "densityplot", height = 200),
+      verbatimTextOutput(outputId = "lmoutput") # regression output
     )
   )
 )
 
+# Define server function required to create the scatterplot
 server <- function(input, output) {
   
-  output$myplot <- renderPlot({
-    plot(rnorm(input$num))
-  }) 
+  # Create the scatterplot object the plotOutput function is expecting
+  output$scatterplot <- renderPlot({
+    ggplot(data = df, aes_string(x = input$x, y = input$y,
+                                     color = input$z)) +
+      geom_point()
+    
   
+  })
+  #creating density plot
+  output$densityplot <- renderPlot({
+    ggplot(data = df, aes_string(x = input$x)) +
+      geom_density()
+  })
+  
+  
+  # Create regression output
+  output$lmoutput <- renderPrint({
+    x <- df %>% pull(input$x)
+    y <- df %>% pull(input$y)
+    summ <- summary(lm(y ~ x, data = df)) 
+    print(summ, digits = 3, signif.stars = FALSE)
+  })
 }
 
-
+# Create a Shiny app object
 shinyApp(ui = ui, server = server)
 
